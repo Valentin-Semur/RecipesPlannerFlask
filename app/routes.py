38102f6cc83fa84181225ae4cc3af127c3c37d07
Valentin-Semur <valentin.semur@gmail.com>
2023-10-21 11:29:01 +0200

    started working on the shopping list creator

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 from app.models import RECIPE_INFO, RECIPE_URLS
 from app.utils import ProcessNewRecipes
+from app.forms import RecipeForm
 from app import db
 
 routes = Blueprint('routes', __name__)
@@ -29,3 +30,12 @@ def new_recipe():
     
     else:
         return render_template('new_recipe.html')
+
+@routes.route('/shopping_list/', methods=['POST', 'GET'])
+def shopping_list():
+    form = RecipeForm()
+    search_string = "%%"
+    if form.validate_on_submit():
+        search_string = "%" + form.input_text.data + "%"
+    recipes = RECIPE_INFO.query.filter(RECIPE_INFO.FLAG_MAIN == 1).filter(RECIPE_INFO.NAME.like(search_string)).all()
+    return render_template('shopping_list.html', form=form, recipes=recipes)
